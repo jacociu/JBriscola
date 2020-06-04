f8e9c8033042892ef7b1deb6ce257cb260c7b36b
Jacociu <jacopociuffetelli@gmail.com>
2020-06-04 15:57:38 +0200

    secondo commit di mazzo

--- src/com/company/Mazzo.java
+++ src/com/company/Mazzo.java
@@ -1,14 +1,50 @@
 package com.company;
 
+import java.util.*;
+
  public class Mazzo {
     //attributi
     private int Size;
 
-    //Costruttore di default: istanzia un oggetto di classe Mazzo settando la grandezza a "40" e inizializzando L'arraylist di carte presenti nel mazzo. Chiama infine il metodo per inizializzare e popolare l'arraylist di carte.}
-    public Mazzo() {
+     public ArrayList<Carta> el = new ArrayList<>();
 
+     //Costruttore di default: istanzia un oggetto di classe Mazzo settando la grandezza a "40" e inizializzando L'arraylist di carte presenti nel mazzo. Chiama infine il metodo per inizializzare e popolare l'arraylist di carte.}
+
+     public Mazzo(){
+        for (Seme s : Seme.values()){
+            for (Valore v : Valore.values())
+
+                el.add(new Carta(v,s));
+        }
+    }
+
+    //get di Size
+    public int GetSize(){
+         return Size;
+    }
+
+    //Metodo Per mischiare il mazzo
+    public void mescola(){
+        Collections.shuffle(el);
+    }
+
+    //metodo per rimuovere una carta dal mazzo
+     public void rimuoviCarta(Carta c){
+
+     }
+
+    // override del metodo toString
+    @Override
+    public String toString(){
+        String s="";
+        for (Carta c :  el){
+
+            s = s+ c+ "\n";
+        }
+        return s;
     }
 
 
 
- }
+
+}
