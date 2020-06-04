c73ac1bb4419f3c7f3eec56aaa69bfec31d42e79
Jacociu <jacopociuffetelli@gmail.com>
2020-06-04 11:32:08 +0200

    primo commit di mazzo

---
+++ src/com/company/Mazzo.java
@@ -0,0 +1,14 @@
+package com.company;
+
+ public class Mazzo {
+    //attributi
+    private int Size;
+
+    //Costruttore di default: istanzia un oggetto di classe Mazzo settando la grandezza a "40" e inizializzando L'arraylist di carte presenti nel mazzo. Chiama infine il metodo per inizializzare e popolare l'arraylist di carte.}
+    public Mazzo() {
+
+    }
+
+
+
+ }
