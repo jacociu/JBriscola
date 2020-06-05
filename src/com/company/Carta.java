22ae9600b4e46f1b54f07bc1e59190ccbf6ee2f3
Jacociu <jacopociuffetelli@gmail.com>
2020-06-05 15:45:59 +0200

    prova giocatore e controllo mazzo

--- src/com/company/Carta.java
+++ src/com/company/Carta.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Objects;
+
 public class Carta {
     //Attributi di com.company.Carta
     private Valore val;
@@ -8,33 +10,39 @@ public class Carta {
     //Costruttore di com.company.Carta
     public Carta(Valore v, Seme s) {
         this.val = v;
-        this.sem =s;
+        this.sem = s;
     }
+
     //Costruttore vuoto
     public Carta() {
     }
+
     //Metodo get di com.company.Valore
     public Valore getValore() {
         return val;
     }
+
     //Metodo get di com.company.Seme
     public Seme getSeme() {
         return sem;
     }
+
     //Metodo toString
     public String toString() {
-        return val + " di "+ sem.toString();
+        return val + " di " + sem.toString();
     }
+
     //Metodo booleano che ci controlla se due carte sono uguali(Stesso seme e stesso valore)
     //"java.lang.Object obj" serve per far confrontare due oggetti
     public boolean equals(java.lang.Object obj)     //(?)
 
     //Metodo toString che ritorna una string di seme e valore come carta
     public String toString() {
-        return val + " di "+ sem.toString();
+        return val + " di " + sem.toString();
     }
+
     //Metodo compareTo Confronta due carte in base al loro com.company.Valore e il loro com.company.Seme
-    public int compareTo(Object o){
+    public int compareTo(Object o) {
         Carta c = (Carta) o;
         if (this.sem.ordinal() < c.sem.ordinal()) return -1;
         if (this.sem.ordinal() > c.sem.ordinal()) return 1;
@@ -42,4 +50,6 @@ public class Carta {
         if (this.val > c.val) return 1;
         else return 0;
     }
+
+
 }
