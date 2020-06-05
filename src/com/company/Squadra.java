0da4fcae54e8fcf36a8cb79ed64df14659117fbd
Prof97 <schienafpio@gmail.com>
2020-06-05 19:15:55 +0200

    Initial commit

--- src/com/company/Squadra.java
+++ src/com/company/Squadra.java
@@ -27,4 +27,21 @@ public class Squadra {
                 ", PartiteVinte=" + PartiteVinte +
                 '}';
     }
+    //DA RIVISIONARE
+    void aggiungiGiocatore(Giocatore nuovoGiocatore) {
+        Calciatore[] nuovoArrayGiocatori = new Calciatore[this.giocatori.length + 1];
+        // copio i giocatori dal vecchio array al nuovo, verificando che non ce ne sia gia' uno con lo stesso nome
+        // inizio dalla posizione 1, lasciando la 0 libera per inserire il nuovo giocatore in prima posizione
+        for (int i=1; i < nuovoArrayGiocatori.length; i++) {
+            int numeroNuovoGiocatore = nuovoCalciatore.numero();
+            int numero = this.giocatori[i-1].numero();
+            if (numero == numeroNuovoGiocatore)
+                throw new RuntimeException("Esiste gia' un giocatore col numero " + numero +"! Inserimento annullato");
+            nuovoArrayGiocatori[i] = this.giocatori[i-1];
+        }
+        // copio il nuovo giocatore nella prima posizione del nuovo array
+        nuovoArrayGiocatori[0] = nuovoCalciatore;
+        // sostituisco il vecchio array con il nuovo array contenente il nuovo giocatore
+        this.giocatori = nuovoArrayGiocatori;
+    }
 }
