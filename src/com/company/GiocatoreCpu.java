2d07f7cebcd349cb547e94cff43d66372dec9931
Jacociu <jacopociuffetelli@gmail.com>
2020-06-06 15:20:09 +0200

    prova giocatore e controllo mazzo

---
+++ src/com/company/GiocatoreCpu.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class GiocatoreCpu extends Giocatore {
+
+    public  void giocaCarta{
+
+
+
+
+    }
+
+
+
+
+
+}
---
+++ src/com/company/GiocatoreReale.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class GiocatoreReale extends Giocatore {
+
+   public void giocaCarta{
+
+
+
+
+    }
+}
