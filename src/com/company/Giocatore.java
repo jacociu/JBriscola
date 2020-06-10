9541ff7c237b6335e5bb9f7ebb4d1becb0e2c083
Jacociu <jacopociuffetelli@gmail.com>
2020-06-10 16:35:11 +0200

    Giocatore Reale e gicatore implemetati

--- src/com/company/Giocatore.java
+++ src/com/company/Giocatore.java
@@ -5,52 +5,97 @@ import java.util.*;
 public abstract class Giocatore {
 
     //attributi
-    private String nome;
-    private Mazzo mano;
-    private Mazzo prese;
-    private Carta cartaGiocata;
-    private Posizione Seduta;
+    protected String nome;
+    protected Mazzo mano;
+    protected Mazzo prese;
+    protected Carta cartaGiocata;//elimina?
+    protected int punti;
+    private Posizione Seduta;// elimina?
 
     //costruttore
-    public Giocatore(String nome, Posizione seduta) {
-        this.nome = nome;
-        Seduta = seduta;
+    public Giocatore(String s){
+        this.nome=s;
+        this.punti=0;
+        mano=new Mazzo();
+        prese=new Mazzo();
     }
-    //metodi get e set
+    /**
+     * il giocatore aggiunge la carta fornitagli dalla partita alla propria presa
+     * @param c carta da aggiungere alle prese
+     */
+    public void presa(Carta c){
 
+        this.prese.aggiungi(c);
+    }
+    /**
+     * restituisce il valore dell'attributo nome
+     * @return il valore dell'attributo nome
+     */
     public String getNome() {
         return nome;
     }
+    /**
+     * modifica il valore dell'attributo nome
+     * @param nome nuovo valore che assumerà l'attributo nome
+     */
     public void setNome(String nome) {
         this.nome = nome;
     }
+    /**
+     * restituisce il valore dell'attributo mano
+     * @return il valore dell'attributo mano
+     */
     public Mazzo getMano() {
-        return mano;
+        return new Mazzo(this.mano);
     }
+    /**
+     * imposta il valore dell'attributo mano
+     * @param mano nuovo valore che assumerà l'attributo mano
+     */
     public void setMano(Mazzo mano) {
-        this.mano = mano;
+        this.mano =new Mazzo(mano);
     }
+    /**
+     * restituisce il valore dell'attributo prese
+     * @return il valore dell'attributo prese
+     */
     public Mazzo getPrese() {
-        return prese;
+        return new Mazzo (this.prese);
     }
+    /**
+     * imposta il valore dell'attributo prese
+     * @param prese nuovo valore che assumerà l'attributo prese
+     */
     public void setPrese(Mazzo prese) {
-        this.prese = prese;
+        this.prese =new Mazzo(prese);
     }
-    public Carta getCartaGiocata() {
-        return cartaGiocata;
+    /**
+     * restituisce il valore dell'attributo punti
+     * @return il valore dell'attributo punti
+     */
+    public int getPunti() {
+        return punti;
     }
-    public void setCartaGiocata(Carta cartaGiocata) {
-        this.cartaGiocata = cartaGiocata;
+    /**
+     * imposta il valore dell'attributo punti
+     * @param punti nuovo valore che assumerà l'attributo punti
+     */
+    public void setPunti(int punti) {
+        this.punti = punti;
     }
-    public Posizione getSeduta() {
-        return Seduta;
-    }
-    public void setSeduta(Posizione seduta) {
-        Seduta = seduta;
-    }
+    /**
+     * metodo astratto definisce la giocata del generico giocatore
+     * @param t carta giocata dal giocatore avversario
+     * @param c briscola
+     * @return la carta da giocare scelta tra quelle in mano
+     */
+    public abstract Carta gioca(Carta t, Carta c);
 
-    //Metodo per ricevere le 3 Carte
-    public abstract void riceviCarte(java.util.ArrayList<Carta> carte);
+
+    //Metodo per prendere una carta
+    public void pesca(Carta c){
+        this.mano.aggiungi(c);}
+
 
 
 
--- src/com/company/GiocatoreReale.java
+++ src/com/company/GiocatoreReale.java
@@ -1,11 +1,68 @@
 package com.company;
 
+import javax.swing.JOptionPane;
+/**
+ * giocatore associato all'utente che utilizza il software
+*/
 public class GiocatoreReale extends Giocatore {
 
-   public void giocaCarta{
-
-
-
+    /**
+     * metodo costruttore richiama il costruttore della superclasse passando il nome del giocatore
+     * @param s nome del giocatore
+     */
+    public GiocatoreReale(String s) {
+        super(s);
 
     }
+
+    /**
+     * l'utente effettua la scelta attraverso un input guidato
+     * @return la carta scelta tra quelle presenti in mano
+     *
+     */
+
+    public Carta gioca(Carta t , Carta c){String r="";int a=0;boolean mossa;
+        do{mossa=true;
+            String text=this.nome+"  la Briscola é"+c.toString()+"\n";
+            if(t==null){
+                text=text.concat("non � ancora stata giocata alcuna carta");}
+            else{text=text.concat("il tuo avversario ha giocato"+t.toString());}
+            text=text.concat("\n in mano hai:\n"+ this.mano.toString()+"Scegli quale carta giocare ");
+            if(this.mano.getMazzo().size()==1){
+                text=text.concat("scrivi 1 per la prima");
+                r=JOptionPane.showInputDialog(text);
+                if(r==null)mossa=false;
+                else{
+                    if(!(r.equals("1")))mossa=false;}
+            }
+            if(this.mano.getMazzo().size()==2){
+                text=text.concat("scrivi 1 per la prima o 2 per la seconda");
+                r=JOptionPane.showInputDialog(text);
+                if(r==null)mossa=false;
+                else{
+                    if(!(r.equals("1")||r.equals("2")))mossa=false;}
+            }
+            if(this.mano.getMazzo().size()==3){text=text.concat("scrivi 1 per la prima,2 per la seconda o 3 per la terza");
+                r=JOptionPane.showInputDialog(text);
+                if(r==null) mossa=false;
+                else{
+                    if(!(r.equals("1")||r.equals("2")||r.equals("3"))){mossa=false; }
+                }
+            }
+
+
+            if(!mossa)
+                JOptionPane.showMessageDialog(null, "LA SCELTA FATTA NON é VALIDA RIPROVA");
+
+        }
+        while(!mossa);
+
+        a=Integer.parseInt(r);
+        Carta ret=this.mano.estrai(a-1);
+        this.mano.scarta(a-1);
+        System.out.println("la carta giocata é:"+ret.toString());
+        return ret;
+
+    }
+
 }
