95facde01886ae6e0b67050619a24a3933b33612
jacociu <38497840+jacociu@users.noreply.github.com>
2020-07-04 15:23:04 +0200

    Update GiocatoreReale.java

--- src/com/company/GiocatoreReale.java
+++ src/com/company/GiocatoreReale.java
@@ -1,16 +1,16 @@
-package com.company;
+package JBriscola;
 
 import javax.swing.JOptionPane;
 /**
  * giocatore associato all'utente che utilizza il software
-*/
+ *
+ */
 public class GiocatoreReale extends Giocatore {
-
     /**
      * metodo costruttore richiama il costruttore della superclasse passando il nome del giocatore
-     * @param s nome del giocatore
+     * @param s nome del giocatore reale
      */
-    public GiocatoreReale(String s) {
+    public GiocatoreReale (String s) {
         super(s);
 
     }
@@ -21,12 +21,22 @@ public class GiocatoreReale extends Giocatore {
      *
      */
 
-    public Carta gioca(Carta t , Carta c){String r="";int a=0;boolean mossa;
+    public Carta gioca(Carta t ,Carta q,Carta l, Carta c){String r="";int a=0;boolean mossa;
         do{mossa=true;
-            String text=this.nome+"  la Briscola é"+c.toString()+"\n";
+            String text=this.nome+"  la Briscola è"+c.toString()+"\n";
             if(t==null){
-                text=text.concat("non � ancora stata giocata alcuna carta");}
-            else{text=text.concat("il tuo avversario ha giocato"+t.toString());}
+                text=text.concat("non è ancora stata giocata alcuna carta");
+            }
+            if(t!=null&&q==null&&l==null) {
+                text=text.concat("la prima carta giocata è "+t.toString());
+            }
+            if(t!=null&&q!=null&&l==null){
+                text=text.concat("la prima carta giocata è "+t.toString()+"e la seconda è "+q.toString());
+            }
+            if(t!=null&&q!=null&&l!=null){
+                text=text.concat("la prima carta giocata è "+t.toString()+", la seconda è "+q.toString()+"e la terza è "+l.toString());
+            }
+
             text=text.concat("\n in mano hai:\n"+ this.mano.toString()+"Scegli quale carta giocare ");
             if(this.mano.getMazzo().size()==1){
                 text=text.concat("scrivi 1 per la prima");
@@ -52,7 +62,7 @@ public class GiocatoreReale extends Giocatore {
 
 
             if(!mossa)
-                JOptionPane.showMessageDialog(null, "LA SCELTA FATTA NON é VALIDA RIPROVA");
+                JOptionPane.showMessageDialog(null, "LA SCELTA FATTA NON è VALIDA, RIPROVA");
 
         }
         while(!mossa);
@@ -60,9 +70,8 @@ public class GiocatoreReale extends Giocatore {
         a=Integer.parseInt(r);
         Carta ret=this.mano.estrai(a-1);
         this.mano.scarta(a-1);
-        System.out.println("la carta giocata é:"+ret.toString());
+        System.out.println("la carta giocata è:"+ret.toString());
         return ret;
 
     }
-
 }
